6dbb9281cac9090f49f277e39b958210436ba2a5
Richard Iyama <richardiyama@yahoo.com>
2020-09-16 16:47:33 +0100

    Initial commit write POST and GET API

---
+++ server/server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+//Middlewares
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+
+//Get -Retrieve data from the server
+app.get("/", (req,res) => {
+
+    res.json("Hello cea shop");
+
+});
+//POST - Send data from frontend to backend
+app.post("/", (req,res) => {
+   console.log(req.body.name);
+});
+
+app.listen(3000, err =>{
+if(err){
+    console.log(err);
+}else{
+    console.log("Listening on port ",3000);
+}
+});
